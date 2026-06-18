486eec91e600953e30c93405f067d408335f297c
NAKADA Nobuhiro <nakada.nobuhiro@gmail.com>
2026-06-19 01:08:00 +0900

    Fix share text format for social media sharing

--- js/script.js
+++ js/script.js
@@ -393,7 +393,7 @@ function shareToX() {
 
     // 2. テキストの組み立て
     // 形式: 日本のGS突破予想確率は 89.5% です。FIFA ワールドカップ... #FIFAWorldCup @nakadanobuhiro
-    const text = `${summaryPrefix} ${probText} ${title} #FIFAWorldCup #wc2026gs @nakadanobuhiro`;
+    const text = `${summaryPrefix} ${probText} ${title} #FIFAWorldCup #wc2026 @nakadanobuhiro`;
 
     // 3. XのシェアURLを開く
     // textパラメータに文章を、urlパラメータにURLを含めることで、カードと文章の両方が表示されやすくなります
